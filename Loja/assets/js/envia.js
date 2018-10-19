b6a0bf4e8f79e161ca40c0fb968d34931af49f33
Rafaaquino <rafaaquino28@gmail.com>
2018-10-19 20:11:57 -0300

    codigo com paginas novas feitas
    1.1

--- Loja/assets/js/envia.js
+++ Loja/assets/js/envia.js
@@ -45,25 +45,13 @@ var addresss = [address];
 
 	var url = 'http://webaquino.com.br';
 	var body = '<?xml version="1.0"?><person><name>Arun</name></person>';
-	//var dadosSerializados = "";
-	//var arrayData = [];
-	//var name;
-
-	// criar um array de pares chave/valor
-	//for(name in data) {
-	//arrayData.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
-	//}
-
-	// Combine the pairs into a single string and replace all %-encoded spaces to 
-	// the '+' character; matches the behaviour of browser form submissions.
-	//dadosSerializados = arrayData.join('&').replace(/%20/g, '+');
 
 
 	XHR.addEventListener('load', function(event) {
-		alert('Dados Enviados!');
+		$('#modalSucesso').modal('toggle');
 	});
 	XHR.addEventListener('error', function(event) {
-		alert('Deu ruim!');
+		$('#modalErro').modal('toggle');
 	});
 
 	XHR.open('POST', 'http://mandanode.herokuapp.com/mandanode/user/create');
@@ -73,121 +61,11 @@ var addresss = [address];
 
 	// Finally, send our data.
 	XHR.send(dataStr);
-}
-
-var enviarProduct = function(ev) {
-ev.preventDefault();
-
-var product = '{"'+ 
-				  'name":"' + document.getElementById('nome-produto').value + 
-				  '", "' +
-				  'idcategory":"' + "1" + 
-				  '", "' +
-				  'brand":"' + document.getElementById('Marca').value + 
-				  '", "' +
-				  'quantity":"' + document.getElementById('qtd').value + 
-				  '", "' +
-				  'code":"' + document.getElementById('codigo').value + 
-				  '", "' +
-				  'measure":"' + document.getElementById('medida').value + 
-				  '", "' +
-				  'description":"' + document.getElementById('descricao').value + 
-				  '", "' +
-				  'cash":"' + document.getElementById('valor').value + 
-				  '"}';
-
-				 				  
-
-	var data = JSON.parse(product);
-		    
-			
-	var XHR = new XMLHttpRequest();
-	//var dadosSerializados = "";
-	//var arrayData = [];
-	//var name;
-
-	// criar um array de pares chave/valor
-	//for(name in data) {
-	//arrayData.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
-	//}
-
-	// Combine the pairs into a single string and replace all %-encoded spaces to 
-	// the '+' character; matches the behaviour of browser form submissions.
-	//dadosSerializados = arrayData.join('&').replace(/%20/g, '+');
 
 
-	XHR.addEventListener('load', function(event) {
-		alert('Dados Enviados!');
-	});
-	XHR.addEventListener('error', function(event) {
-		alert('Deu ruim!');
-	});
-
-	XHR.open('POST', 'http://mandanode.herokuapp.com/mandanode/product/create');
-
-	// header HTTP para dados de formulário POST
-	XHR.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-
-	// Finally, send our data.
-	XHR.send(product);
-}
+	setTimeout(function (){window.location.href = "escolha-pagamento.html";}, 5000);
 
 
 
-var enviarUser = function(ev) {
-ev.preventDefault();
-
-var user = '{"'+ 
-				  'name":"' + document.getElementById('nome').value + 
-				  '", "' +
-				  'email":"' + document.getElementById('email').value + 
-				  '", "' +
-				  'password":"' + document.getElementById('senha').value + 
-				  '", "' +
-				  'dateBirth":"' + document.getElementById('data').value + 
-				  '", "' +
-				  'sex":"' + document.getElementById('sexo').value + 
-				  '", "' +
-				  'cpf":"' + document.getElementById('cpf').value + 
-				  '", "' +
-				  'phone":"' + document.getElementById('celular').value + 
-				  '"}';
-
-				 				  
-
-	var data = JSON.parse(user);
-		    
-			
-	var XHR = new XMLHttpRequest();
-	//var dadosSerializados = "";
-	//var arrayData = [];
-	//var name;
-
-	// criar um array de pares chave/valor
-	//for(name in data) {
-	//arrayData.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
-	//}
-
-	// Combine the pairs into a single string and replace all %-encoded spaces to 
-	// the '+' character; matches the behaviour of browser form submissions.
-	//dadosSerializados = arrayData.join('&').replace(/%20/g, '+');
-
-
-	XHR.addEventListener('load', function(event) {
-		alert('Dados Enviados!');
-	});
-	XHR.addEventListener('error', function(event) {
-		$( function() {
-	    	$( "#modalErro" ).dialog();
-	  } );
-	});
-
-	XHR.open('POST', 'http://mandanode.herokuapp.com/mandanode/user/create');
-
-	// header HTTP para dados de formulário POST
-	XHR.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-
-	// Finally, send our data.
-	XHR.send(user);
 }
 
