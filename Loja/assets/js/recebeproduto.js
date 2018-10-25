4378f7048d8119281b6e254c5e3132c9debb234b
Rafaaquino <rafaaquino28@gmail.com>
2018-10-25 15:32:34 -0300

    função produto json funcioando

--- Loja/assets/js/recebeproduto.js
+++ Loja/assets/js/recebeproduto.js
@@ -17,15 +17,37 @@ $(document).ready(function () {
 
     function listarProdutos() {
         
-        var currentPage = 1;
+        $('#lista-produtos').html('');
+
         for (var i = 0; i < produtos.length; i++) {
             var produto = produtos[i];
            
-                $('#nomeProduto').append(`<h3 id="nomeProduto" class="card-text"><a href="#${produto.id}">${produto.brand}</a> </h3>`);
+             $('#lista-produtos').append(`
+             		<div class="col-md-4" >
+              
+		              <div class="card mb-4 box-shadow">
+		                <img  class="card-img-top img-ajuste"  alt="camisa branca" title="camisa branca" style="height: 225px; width: 100%; display: block;" src="assets/img/produtos/camisa.jpg" id="imgProduto">
+		                <div class="card-body">
+		                  <h3 id="nomeProduto" class="card-text">${produto.name}</h3>
+		                  <div class="d-flex justify-content-between align-items-center">
+		                    <div class="btn-group">
+		                      <a  href="produto.html" class="btn btn-sm btn-outline-secondary">Comprar</a>
+		                    </div>
+		                    <h4 id="preco-produto" class="text-muted">R$ ${produto.cash},00</h4>
+		                  </div>
+		                </div>
+		              </div>
+
+		            </div>
+
+             	`); 
+
+
+               // $('#nomeProduto').append(`<h3 id="nomeProduto" class="card-text"><a href="#${produto.id}">${produto.brand}</a> </h3>`);
             
-                $('#preco-produto').append(`<h4 id="preco-produto" class="text-muted">R$ ${produto.cash}</h4>`);
+                //$('#preco-produto').append(`<h4 id="preco-produto" class="text-muted">R$ ${produto.cash}</h4>`);
            
-            currentPage ++;
+            
         }
       
     }
